126b12261f87f1b27c665df89c78006f8a4cfb05
Pablo Cunha <p.abloc1@hotmail.com>
2021-10-14 02:41:05 -0300

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Header = () => {
+    return (
+        <div className="header-wraper">
+            <div className="main-info">
+                <h1>desenvolvimento web</h1>
+                <Typed 
+                    className="typed-text"
+                    strings= { ["React", "Angular", "VueJs", "CSS"] }
+                    typeSpeed= {40}
+                    backSpeed= {60}
+                    loop 
+                />
+                <a href="#" className="btn-main-offer">contacte-me</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,6 @@ const Navbar = () => {
     return (       
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
         <div className="container">
-  <div className="container-fluid">
     <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo"/></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }}/>
@@ -29,7 +28,6 @@ const Navbar = () => {
         </li>
       </ul>
     </div>
-  </div>
   </div>
 </nav>
     )
