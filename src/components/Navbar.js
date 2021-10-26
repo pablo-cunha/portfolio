1be512f078b0d3f0aae136dfeb5a2ddb51dfad33
Pablo Cunha <p.abloc1@hotmail.com>
2021-10-25 22:42:59 -0300

    Portfolio

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
     return (       
         <nav className="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
         <div className="container">
-    <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo"/></a>
+    <a className="navbar-brand" href="/"><img className="logo" src={logo} alt="logo"/></a>
     <h3 className="navbar-name">PABLO CUNHA</h3>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }}/>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import crud from '../projeto1.JPG'
-import galeria from '../galeria.JPG'
+import toDoList from '../vue-task-manager.JPG'
+import netflix from '../netflix-clone.JPG'
 // REACT POP UP BOX
 import { PopupboxManager, PopupboxContainer } from "react-popupbox"
 import "react-popupbox/dist/react-popupbox.css"
@@ -10,51 +10,51 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Portfolio = () => {
 
-    // Projeto CRUD
-    const openPopupboxCRUD = () => {
+    // Projeto Netflix
+    const openPopupboxNetflix = () => {
         const content = (
         <>
-        <a href="">            
-        <img className="popup-image" src={crud} alt="Sistema CRUD"/> 
+        <a href="https://react-netflix-khaki.vercel.app/">            
+        <img className="popup-image" src={netflix} alt="Clone Netflix"/> 
         </a>
-        <p>Sistema CRUD em React, TailwindCSS e FireBase.</p>
+        <p>Clone Frontend Netflix desenvolvido em React.</p>
         <p>Clique na imagem e acesse o projeto!</p>
-        <b>Github: </b> <a className="hyper-link" onClick={() => window.open("https://github.com/pablo-cunha/sistemaCrudComReact") }>
-            https://github.com/pablo-cunha/sistemaCrudComReact</a>
+        <b>Github: </b> <a className="hyper-link" onClick={() => window.open("https://github.com/pablo-cunha/reactNetflix") } href="">
+        https://github.com/pablo-cunha/reactNetflix</a>
         </>
         )
         PopupboxManager.open({content})
     } 
 
-    const popupboxConfigCRUD = {
+    const popupboxConfigNetflix = {
         titleBar: {
             enable: true,
-            text: "Sistema CRUD"
+            text: "Clone Netflix"
         },
         fadeIn: true,
         fadeInSpeed: 500
     }
 
-        // Projeto Galeria
-        const openPopupboxGaleria = () => {
+        // Projeto Vue
+        const openPopupboxToDo = () => {
             const content = (
             <>
-            <a href="">            
-            <img className="popup-image" src={galeria} alt="Galeria de Fotos"/> 
+            <a href="https://vue-todo-list-rho.vercel.app/">            
+            <img className="popup-image" src={toDoList} alt="Task Manager"/> 
             </a>
-            <p>Galeria de fotos com filtro, feito com Webpack e Bootstrap.</p>
+            <p>To Do List em VueJS e estilizado com Bootstrap.</p>
             <p>Clique na imagem e acesse o projeto!</p>
-            <b>Github: </b> <a className="hyper-link" onClick={() => window.open("https://github.com/pablo-cunha/projetoGaleriaDeFotos") }>
-                https://github.com/pablo-cunha/projetoGaleriaDeFotos</a>
+            <b>Github: </b> <a className="hyper-link" onClick={() => window.open("https://github.com/pablo-cunha/vueToDoList") } href="">
+            https://github.com/pablo-cunha/vueToDoList</a>
             </>
             )
             PopupboxManager.open({content})
         } 
     
-        const popupboxConfigGaleria = {
+        const popupboxConfigToDo = {
             titleBar: {
                 enable: true,
-                text: "Galeria de Fotos"
+                text: "To-do List"
             },
             fadeIn: true,
             fadeInSpeed: 500
@@ -65,23 +65,21 @@ const Portfolio = () => {
             <div className="container">
                 <h1 className="text-uppercase text-center py-5">portfolio</h1>
                 <div className="image-box-wrapper row justify-content-center">
-                    <div className="portfolio-image-box" onClick={openPopupboxCRUD}>
-                    <img className="portfolio-image" src={crud} alt="Sistema CRUD em React e 
-                    TailwindCSS"/>
+                    <div className="portfolio-image-box" onClick={openPopupboxNetflix}>
+                    <img className="portfolio-image" src={netflix} alt="Clone Netflix desenvolvido em React."/>
                     <div className="overflow"></div>
                     <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus}/>
                     </div>
                 {/* - */}
-                    <div className="portfolio-image-box" onClick={openPopupboxGaleria}>
-                    <img className="portfolio-image" src={galeria} alt="Galeria de Fotos com filtro
-                    com Webpack e Bootstrap"/>
+                    <div className="portfolio-image-box" onClick={openPopupboxToDo}>
+                    <img className="portfolio-image" src={toDoList} alt="Task Manager feito em VueJS e Bootstrap"/>
                     <div className="overflow"></div>
                     <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus}/>
                     </div>
                 </div>
             </div>
-            <PopupboxContainer {...popupboxConfigCRUD}/>
-            <PopupboxContainer {...popupboxConfigGaleria}/>
+            <PopupboxContainer {...popupboxConfigNetflix}/>
+            <PopupboxContainer {...popupboxConfigToDo}/>
     </div>
     )
 }
