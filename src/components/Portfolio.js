789cccbaf3969d0681db333878a2c5fbea386a87
Pablo <lcunha.pablo@gmail.com>
2022-06-20 18:34:58 -0300

    Portfolio atualizado

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -73,7 +73,7 @@ const Portfolio = () => {
                 <a href="https://github-search-zeta-opal.vercel.app/" target="_blank" rel="noreferrer">            
                 <img className="popup-image" src={githubSearch} alt="Github Search"/> 
                 </a>
-                <p>Buscador de perfis Github que exibe detalhes sobre o perfil pesquisado. Desenvolvido em Reactjs.</p>
+                <p>Buscador de perfis Github que retorna detalhes sobre o perfil pesquisado. Desenvolvido em Reactjs.</p>
                 <p>Clique na imagem e acesse o projeto!</p>
                 <b>Github: </b> <a className="hyper-link" onClick={() => window.open("https://github.com/pablo-cunha/github-search") } 
                 href="" target="_blank" rel="noreferrer">
