fbf11fabaf18bd414215759a53f77cc3a5b98ffd
Pablo <lcunha.pablo@gmail.com>
2022-06-20 18:53:12 -0300

    Atualização

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -40,32 +40,6 @@ const Portfolio = () => {
         fadeInSpeed: 500
     }
 
-    // Wordle Clone
-    const openPopupboxWordle = () => {
-        const content = (
-            <>
-            <a href="https://pablo-cunha.github.io/wordle-clone/" target="_blank" rel="noreferrer">            
-                <img className="popup-image" src={wordleClone} alt="Wordle Clone"/> 
-            </a>
-            <p>Clone do jogo Wordle, feito com HTML, CSS e JavaScript.</p>
-            <p>Clique na imagem e acesse o projeto!</p>
-            <b>Github: </b> <a className="hyper-link" onClick={() => window.open("https://github.com/pablo-cunha/wordle-clone") } 
-            href="" target="_blank" rel="noreferrer">
-            https://github.com/pablo-cunha/wordle-clone</a>
-            </>
-            )
-            PopupboxManager.open({content})
-        }
-
-    const popupboxConfigWordle = {
-        titleBar: {
-            enable: true,
-            text: ""
-        },
-        fadeIn: true,
-        fadeInSpeed: 500
-    }
-
     // Github Search
     const openPopupboxGitSearch = () => {
             const content = (
@@ -73,7 +47,7 @@ const Portfolio = () => {
                 <a href="https://github-search-zeta-opal.vercel.app/" target="_blank" rel="noreferrer">            
                 <img className="popup-image" src={githubSearch} alt="Github Search"/> 
                 </a>
-                <p>Buscador de perfis Github que retorna detalhes sobre o perfil pesquisado. Desenvolvido em Reactjs.</p>
+                <p>Buscador de perfis Github que exibe detalhes sobre o perfil pesquisado. Desenvolvido em Reactjs.</p>
                 <p>Clique na imagem e acesse o projeto!</p>
                 <b>Github: </b> <a className="hyper-link" onClick={() => window.open("https://github.com/pablo-cunha/github-search") } 
                 href="" target="_blank" rel="noreferrer">
@@ -118,6 +92,33 @@ const Portfolio = () => {
         fadeInSpeed: 500
     }
 
+    // Wordle Clone
+    const openPopupboxWordle = () => {
+        const content = (
+            <>
+            <a href="https://pablo-cunha.github.io/wordle-clone/" target="_blank" rel="noreferrer">            
+                <img className="popup-image" src={wordleClone} alt="Wordle Clone"/> 
+            </a>
+            <p>Clone do jogo Wordle, feito com HTML, CSS e JavaScript.</p>
+            <p>Clique na imagem e acesse o projeto!</p>
+            <b>Github: </b> <a className="hyper-link" onClick={() => window.open("https://github.com/pablo-cunha/wordle-clone") } 
+            href="" target="_blank" rel="noreferrer">
+            https://github.com/pablo-cunha/wordle-clone</a>
+            </>
+            )
+            PopupboxManager.open({content})
+        }
+
+    const popupboxConfigWordle = {
+        titleBar: {
+            enable: true,
+            text: ""
+        },
+        fadeIn: true,
+        fadeInSpeed: 500
+    }
+
+
     return (
         <div id="portfolio" className="portfolio-wrapper">
             <div className="container">
@@ -126,12 +127,6 @@ const Portfolio = () => {
                 {/* Netflix Box */}
                     <div className="portfolio-image-box" onClick={openPopupboxNetflix}>
                     <img className="portfolio-image" src={netflix} alt="Clone Netflix desenvolvido em React."/>
-                    <div className="overflow"></div>
-                    <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus}/>
-                    </div>
-                {/* Wordle Clone Box */}
-                    <div className="portfolio-image-box" onClick={openPopupboxWordle}>
-                    <img className="portfolio-image" src={wordleClone} alt="Clone do jogo Wordle, feito com HTML, CSS e JavaScript."/>
                     <div className="overflow"></div>
                     <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus}/>
                     </div>
@@ -144,6 +139,12 @@ const Portfolio = () => {
                 {/* Bikcraft Box */}
                     <div className="portfolio-image-box" onClick={openPopupboxBikcraft}>
                     <img className="portfolio-image" src={bikcraft} alt="Projeto site de bicicletas elétricas, feito apenas com HTML, CSS e JavaScript."/>
+                    <div className="overflow"></div>
+                    <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus}/>
+                    </div>
+                {/* Wordle Clone Box */}
+                <div className="portfolio-image-box" onClick={openPopupboxWordle}>
+                    <img className="portfolio-image" src={wordleClone} alt="Clone do jogo Wordle, feito com HTML, CSS e JavaScript."/>
                     <div className="overflow"></div>
                     <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus}/>
                     </div>
